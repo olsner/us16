b152cfd6f73857999b7264e19fa29c5847a62443
Simon Brenner <olsner@gmail.com>
2016-10-19 20:57:22 +0200

    Detab

--- usctl.c
+++ usctl.c
@@ -64,7 +64,7 @@
 
 /*
 // Out-channel 1, set to Master L (I think)
-	CTLMSGHEAD with 0x03 instead of 0x04
+    CTLMSGHEAD with 0x03 instead of 0x04
 61 02 03 62 02 01 41 01
                ^ channel = 1
                   ^ controller = 41 (probably 41..48?)
@@ -105,31 +105,31 @@ static libusb_device *dev;
 static int was_kernel_active;
 
 #define LIBUSB_ERROR(err, msg) \
-	do { \
-		fprintf(stderr, "libusb error %d: %s in %s\n", err, libusb_error_name(err), msg); \
-	} while (0)
+    do { \
+        fprintf(stderr, "libusb error %d: %s in %s\n", err, libusb_error_name(err), msg); \
+    } while (0)
 #define CHECK_LIBUSB(expr) \
-	do { \
-		int e__ = (expr); \
-		if (e__) { \
-			LIBUSB_ERROR(e__, #expr); \
+    do { \
+        int e__ = (expr); \
+        if (e__) { \
+            LIBUSB_ERROR(e__, #expr); \
             abort(); \
-		} \
-	} while (0)
+        } \
+    } while (0)
 
 #define LIBUSB_(func, ...) \
-	CHECK_LIBUSB(libusb_##func(__VA_ARGS__))
+    CHECK_LIBUSB(libusb_##func(__VA_ARGS__))
 
 const int CTLMSG_TIMEOUT = 1000;
 
 void send_urb(const char* buf, int size) {
-	int res = libusb_control_transfer(h,
-			SND_US16X08_URB_REQUEST,
-			SND_US16X08_URB_REQUESTTYPE,
-			0, 0,
-			(char *)buf,
-			size,
-			CTLMSG_TIMEOUT);
+    int res = libusb_control_transfer(h,
+            SND_US16X08_URB_REQUEST,
+            SND_US16X08_URB_REQUESTTYPE,
+            0, 0,
+            (char *)buf,
+            size,
+            CTLMSG_TIMEOUT);
     if (res != size) {
         LIBUSB_ERROR(res, "send_urb");
     }
@@ -139,52 +139,52 @@ void send_urb(const char* buf, int size) {
 // controller: mixer constant above,
 // value: 0..255 or 0..1 or -128..127
 void mix(int channel, int controller, int value) {
-	char buf[] = {
-		CTLMSGHEAD,
-		channel,
-		controller,
-		0x02, //                    0x07: unknown
-		value,
-		0x00, 
-		0x00
-	};
+    char buf[] = {
+        CTLMSGHEAD,
+        channel,
+        controller,
+        0x02, //                    0x07: unknown
+        value,
+        0x00, 
+        0x00
+    };
 
-	send_urb(buf, sizeof(buf));
+    send_urb(buf, sizeof(buf));
 }
 
 int main() {
-	LIBUSB_(init, &ctx);
-	libusb_set_debug(ctx, 3);
+    LIBUSB_(init, &ctx);
+    libusb_set_debug(ctx, 3);
 
-	h = libusb_open_device_with_vid_pid(ctx, 0x0644, 0x8047);
-	if (!h) {
-		fprintf(stderr, "No US-16x08 found.\n");
-		abort();
-	}
-	dev = libusb_get_device(h);
-	if (!dev) {
-	}
-	printf("Found US-16x08 at %d:%d\n",
-			libusb_get_bus_number(dev),
-			libusb_get_device_address(dev));
-
-	// US16x08 URBs are send to "control pipe" (linux usb thing?) for endpoint
-	// 0. But which interface?
-
-	const int IFACENUM = 0;
-
-	was_kernel_active = libusb_kernel_driver_active(h, IFACENUM);
-	if (!(was_kernel_active == 0 || was_kernel_active == 1)) {
-		LIBUSB_ERROR(was_kernel_active, "driver_active");
+    h = libusb_open_device_with_vid_pid(ctx, 0x0644, 0x8047);
+    if (!h) {
+        fprintf(stderr, "No US-16x08 found.\n");
         abort();
-	}
-	if (was_kernel_active) {
-		LIBUSB_(detach_kernel_driver, h, IFACENUM);
-		printf("Detached kernel driver.\n");
-	}
+    }
+    dev = libusb_get_device(h);
+    if (!dev) {
+    }
+    printf("Found US-16x08 at %d:%d\n",
+            libusb_get_bus_number(dev),
+            libusb_get_device_address(dev));
 
-	mix(9, SND_US16X08_ID_FADER, 255);
-	mix(9, SND_US16X08_ID_MUTE, 0);
+    // US16x08 URBs are send to "control pipe" (linux usb thing?) for endpoint
+    // 0. But which interface?
+
+    const int IFACENUM = 0;
+
+    was_kernel_active = libusb_kernel_driver_active(h, IFACENUM);
+    if (!(was_kernel_active == 0 || was_kernel_active == 1)) {
+        LIBUSB_ERROR(was_kernel_active, "driver_active");
+        abort();
+    }
+    if (was_kernel_active) {
+        LIBUSB_(detach_kernel_driver, h, IFACENUM);
+        printf("Detached kernel driver.\n");
+    }
+
+    mix(9, SND_US16X08_ID_FADER, 255);
+    mix(9, SND_US16X08_ID_MUTE, 0);
 
 #define MSGCAT 0x61, 0x02,
 #define MSGCH 0x62, 0x02,
@@ -205,11 +205,11 @@ int main() {
 
     send_urb(msg, sizeof(msg));
 
-	if (was_kernel_active) {
-		printf("Done, reattaching kernel driver.\n");
-		LIBUSB_(attach_kernel_driver, h, IFACENUM);
-	}
+    if (was_kernel_active) {
+        printf("Done, reattaching kernel driver.\n");
+        LIBUSB_(attach_kernel_driver, h, IFACENUM);
+    }
 
-	libusb_close(h);
-	libusb_exit(ctx);
+    libusb_close(h);
+    libusb_exit(ctx);
 }
